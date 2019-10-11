da344e3cf511004d1e0b135602d63403084f7e50
hossein <mrfathi4@gmail.com>
2019-10-11 16:29:51 +0330

    Remove ignored files

--- web/models.py
+++ web/models.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,4 +10,17 @@ class Expense (models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} --- {} ---> {}".format(self.date, self.text, self.amount)
+
+
+class Income (models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return "{} --- {} ---> {}".format(self.date, self.text, self.amount)
