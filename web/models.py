62de92a4badd1f1609a259dc643950cddd0da3d3
hossein <mrfathi4@gmail.com>
2019-10-20 15:25:55 +0330

    add temp_user model

--- web/models.py
+++ web/models.py
@@ -3,10 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)  # TODO: do not save password
 
-class Expense (models.Model):
+
+class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
@@ -16,7 +24,7 @@ class Expense (models.Model):
         return "{} --- {} ---> {}".format(self.date, self.text, self.amount)
 
 
-class Income (models.Model):
+class Income(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
@@ -29,5 +37,6 @@ class Income (models.Model):
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=45)
+
     def __str__(self):
         return '{} -- token'.format(self.user)
